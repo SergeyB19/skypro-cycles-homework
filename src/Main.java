20a7fa34b558de9485989808b2b69d331986ec56
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-05-24 15:11:32 +0300

    ### Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         int currentNum = 0;
@@ -9,9 +11,25 @@ public class Main {
 
         for (; currentNum > 0; currentNum--) {
             System.out.print(currentNum + " ");
+            // задание №2
+            int firstFriday = 5;
+            for (int currentFriday = firstFriday; currentFriday < 31; currentFriday += 7) {
+                System.out.println("Сегодня пятница " + currentFriday + "-е число. Необходимо подготовить отчет.");
+                // Задание 3
+                int currentYear = LocalDateTime.now().getYear();
+
+                int start = currentYear - 200;
+                int end = currentYear + 100;
+
+                for (int year = start; year < end; year++) {
+                    if (year % 79 == 0) {
+                        System.out.println(year);
+                    }
+                }
+            }
         }
         }
-    }
+        }
 
 
 
